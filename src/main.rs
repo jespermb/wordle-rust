8680e64c6c01825d0bcff638f522433cda6e2592
Jesper Bisgaard <jesperjb@gmail.com>
2022-04-21 08:12:56 +0200

    Added test for choose word.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use rand::seq::SliceRandom;
 
+/// Generate a random word
+///
+/// # Examples
+///
+/// ```
+/// let word = choose_word();
+/// ```
 fn choose_word() -> String {
     let words: Vec<&str> = vec!["apple", "green", "brown", "elder"];
     let chosen_word: Option<&&str> = words.choose(&mut rand::thread_rng());
     return chosen_word.unwrap().to_string();
+}
+#[test]
+fn test_choose_word() {
+    let word = choose_word();
+    assert_eq!(word.chars().count(), 5);
 }
 
 fn main() {
